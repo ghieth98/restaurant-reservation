ed4df2b4b2a85fdb29be4d2fad6878cdc8d84f40
hossam ghieth <hossamghieth@gmail.com>
2023-02-28 13:37:27 +0200

    Integrated sail

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -139,7 +139,7 @@
                 TODAY'S SPECIALITY</h2>
         </div>
         <div class="container w-full px-5 py-6 mx-auto">
-            <div class="grid lg:grid-cols-4 gap-y-6">
+            <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-y-6">
                 @foreach($specials as $special)
                     <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
                         <img class="w-full h-48"
@@ -210,14 +210,14 @@
     {{--    </section>--}}
     <!-- End Gallery section -->
 
-    <!-- Testimonial Section -->
+    <!-- Test   imonial Section -->
     <section class="pt-4 pb-12 bg-gray-800">
         <div class="my-16 text-center">
             <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
                 Testimonial </h2>
             <p class="text-xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. soluta sapient</p>
         </div>
-        <div class="grid gap-2 lg:grid-cols-3">
+        <div class="grid gap-2 md:grid-cols-2 lg:grid-cols-3 ml-4">
             <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
                 <div class="flex justify-center -mt-16 md:justify-end">
                     <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full"
@@ -274,68 +274,4 @@
     </section>
     <!-- End Testimonial Section -->
 
-    <!-- Order now section -->
-    {{--    <section class="pt-12 pb-12 bg-red-50">--}}
-    {{--        <div class="container flex items-center justify-center p-6 mx-auto bg-white shadow-lg sm:p-12 md:w-1/2">--}}
-    {{--            <div class="w-full">--}}
-
-    {{--                <h1--}}
-    {{--                    class="mb-4 text-2xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">--}}
-    {{--                    Order Now FREE AND FAST--}}
-    {{--                </h1>--}}
-    {{--                <div class="gap-2 mb-8 lg:flex">--}}
-    {{--                    <div class="w-full">--}}
-    {{--                        <label class="block text-base">--}}
-    {{--                            Your Name--}}
-    {{--                        </label>--}}
-    {{--                        <input type="text"--}}
-    {{--                               class="w-full px-4 py-2 text-base border rounded-md focus:border-green-400 focus:outline-none focus:ring-1 focus:ring-green-600"--}}
-    {{--                               placeholder="Your Name"/>--}}
-    {{--                    </div>--}}
-    {{--                    <div class="w-full">--}}
-    {{--                        <label class="block text-base">--}}
-    {{--                            Your Number--}}
-    {{--                        </label>--}}
-    {{--                        <input type="text"--}}
-    {{--                               class="w-full px-4 py-2 text-base border rounded-md focus:border-green-400 focus:outline-none focus:ring-1 focus:ring-green-600"--}}
-    {{--                               placeholder="Your Number"/>--}}
-    {{--                    </div>--}}
-    {{--                </div>--}}
-    {{--                <div class="gap-2 mb-8 lg:flex">--}}
-    {{--                    <div class="w-full">--}}
-    {{--                        <label class="block text-base">--}}
-    {{--                            Table Number--}}
-    {{--                        </label>--}}
-    {{--                        <input type="text"--}}
-    {{--                               class="w-full px-4 py-2 text-base border rounded-md focus:border-green-400 focus:outline-none focus:ring-1 focus:ring-green-600"--}}
-    {{--                               placeholder="Enter Table Number 1 to 12"/>--}}
-    {{--                    </div>--}}
-    {{--                    <div class="w-full">--}}
-    {{--                        <label class="block text-base">--}}
-    {{--                            Booking Date--}}
-    {{--                        </label>--}}
-    {{--                        <input type="date"--}}
-    {{--                               class="w-full px-4 py-2 text-base border rounded-md focus:border-green-400 focus:outline-none focus:ring-1 focus:ring-green-600"--}}
-    {{--                               placeholder="Your Number"/>--}}
-    {{--                    </div>--}}
-    {{--                </div>--}}
-    {{--                <div class="">--}}
-    {{--                    <label class="block text-base">--}}
-    {{--                        Your Message--}}
-    {{--                    </label>--}}
-    {{--                    <textarea name="" id="" rows="8" cols="30"--}}
-    {{--                              class="w-full p-3 text-base border rounded-md focus:border-green-400 focus:outline-none focus:ring-1 focus:ring-green-600"--}}
-    {{--                              placeholder=""></textarea>--}}
-    {{--                </div>--}}
-    {{--                <button--}}
-    {{--                    class="px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-green-600 border border-transparent rounded-lg active:bg-green-600 hover:bg-green-700 focus:outline-none focus:shadow-outline-green"--}}
-    {{--                    href="#">--}}
-    {{--                    Book Now--}}
-    {{--                </button>--}}
-
-    {{--            </div>--}}
-    {{--        </div>--}}
-    {{--    </section>--}}
-
-    <!-- End Order now Section -->
 </x-guest-layout>
